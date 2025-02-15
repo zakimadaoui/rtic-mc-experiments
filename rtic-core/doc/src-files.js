18b924ed952d94aa0d9603506a43d571d2fea748
zakimadaoui <zakimadaoui027@gmail.com>
2025-02-15 20:49:02 +0200

    refactoring: move compilation passes to a  directory

--- rtic-core/doc/src-files.js
+++ rtic-core/doc/src-files.js
@@ -1,4 +1,3 @@
-var srcIndex = new Map(JSON.parse('[\
-["rtic_core",["",[["analysis",[],["mod.rs"]],["codegen",[],["hw_task.rs","mod.rs","shared_resources.rs","task_init.rs","utils.rs"]],["common_internal",[],["mod.rs","rtic_functions.rs","rtic_traits.rs"]],["parse_utils",[],["mod.rs"]],["parser",[],["ast.rs","mod.rs"]]],["backend.rs","errors.rs","lib.rs"]]]\
-]'));
+var srcIndex = new Map(JSON.parse('[["rtic_core",["",[["analysis",[],["mod.rs"]],["codegen",[],["hw_task.rs","mod.rs","shared_resources.rs","task_init.rs","utils.rs"]],["common_internal",[],["mod.rs","rtic_functions.rs","rtic_traits.rs"]],["parse_utils",[],["mod.rs"]],["parser",[],["ast.rs","mod.rs"]]],["backend.rs","errors.rs","lib.rs"]]]]'));
 createSrcSidebar();
+//{"start":36,"fragment_lengths":[305]}
--- rtic-core/doc/trait.impl/core/error/trait.Error.js
+++ rtic-core/doc/trait.impl/core/error/trait.Error.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"rtic_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"rtic_core/errors/enum.ParseError.html\" title=\"enum rtic_core::errors::ParseError\">ParseError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["rtic_core",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"rtic_core/errors/enum.ParseError.html\" title=\"enum rtic_core::errors::ParseError\">ParseError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[287]}
